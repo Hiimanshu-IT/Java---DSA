7a8a03bd71ba608e436efc529cccd602ac23f9c4
Himanshu Thakur <3006himanshuthakur@gmail.com>
2026-04-10 12:46:29 +0530

    Delete naturalno.java

--- naturalno.java
+++
@@ -1,9 +0,0 @@
-public class naturalno {
-    public static void main(String[] args) {
-        short i = 1;
-        do{
-            System.out.println(i);
-            i++;
-        }while(i>0);
-    }
-}
